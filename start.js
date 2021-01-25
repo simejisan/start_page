b01057bb1aa0702810b7f6838092174b8e2839f1
Le Tam <letam.tamle@gmail.com>
2021-01-25 17:29:45 +0700

    main structure

---
+++ start.js
@@ -0,0 +1,11 @@
+var mainContent = $(".main");
+var happyMascos = $(".main .mascos .happy");
+var wowMascos = $(".wow");
+
+function showMain(){
+    $(".main").delay(1000).fadeIn();
+}
+$(document).ready(function () {
+showMain();
+// hide();
+});
